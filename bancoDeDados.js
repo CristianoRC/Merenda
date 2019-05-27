4d9b62300c480363062696d1c8f87ebc5c493c12
Cristiano Cunha <contato@cristianoprogramador.com>
2019-05-27 17:55:47 -0300

    Adiciona o arquivo para conexão no banco

---
+++ bancoDeDados.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+let conexao = mysql.createConnection(
+    {
+        host: 'mysql465.umbler.com',
+        port: '41890',
+        database: 'merenda',
+        user: 'merenda',
+        password: 'merenda2019',
+        charset: 'utf8'
+    });
+
+module.exports = {
+    conexao: conexao,
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const api = express();
+api.use(cors());
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json());
+
+// ======================= Importação dos controllers =======================//
+
+require('./Controllers/exemplo')(api);
+
+// =========================================================================//
+
+const porta = 3000;
+api.listen(porta, () => {
+    console.log(`Api está rodando na porta ${porta}`);
+})
