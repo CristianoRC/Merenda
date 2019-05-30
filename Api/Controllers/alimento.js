fbc149ebf2d051a042e09444c43a88ad68a9244d
Guilherme Rodrigues Correa <rodsguilherme@gmail.cm>
2019-05-30 19:12:40 -0300

    alimento fatorado (não funciona ainda)

--- Api/Controllers/alimento.js
+++ Api/Controllers/alimento.js
@@ -50,38 +50,51 @@ router.post('/', (request, response) => {
 
 });
 
-/* /api/alimento/{id} : PUT - Atualiza os dados do alimento.
-Se tudo ok 200
-Se der algum problema 400
-Se o Id não existir 404
-*/
 
+
+// Cadastra um novo Alimento 
+router.post('/', (request, response) => {
+    // Se o user preencher os campos vai entrar no IF
+    if (request.body.nome && request.body.descricao && request.body.categoria >= 0) {
+        bancoDeDados.conexao.query(`insert into Alimento (Nome, Descricao, Categoria) values('${request.body.nome}', '${request.body.descricao}', '${request.body.categoria}')`,
+            (erro, resultado) => {
+                if (!erro) // Se não tiver erro
+                    response.status(200);
+                else
+                    response.status(400).json({ Erro: erro });
+            }
+
+        )
+    }
+    // Se o user não mandar os campos retorna 400
+    else
+        response.status(400).json({ Erro: 'Preencha os campos de Nome, Descrição e Categoria!' });
+    // Manda a request p/ o user
+    response.send();
+
+});
+
+
+// Atualiza os dados do Alimento
 router.put('/:id', (request, response) => {
-
-    if(request.params.id) {
         bancoDeDados.conexao.query(`select * from Alimento where Id = ${request.params.id}`),
         (erro, resultado) => {
-            if(resultado.length == 0)
+            if(resultado.length === 0)
                 response.status(404);
-            if(!erro) {
+            if(!erro) 
                 bancoDeDados.conexao.query(`update Alimento set Nome = '${request.body.nome}', 
                 Descricao = '${request.body.descricao}', Categoria = '${request.body.categoria}' where Id = ${request.params.id}`, (erro, resultado) =>{
-                     if(erro) 
+                   if(erro) {
                         response.status(400).json({ erro: erro });
-                     else 
+                    } else 
                         response.status(200);                
                    
                 })
-            } 
-            else 
+            else
                 response.status(400).json({ Erro: erro }); 
-        }
-    }    
-    else 
-        response.status(400);
-
-        response.send();
-});
+            } 
+           
+        });
 
 
 module.exports = (api) => api.use('/api/alimento', router);
