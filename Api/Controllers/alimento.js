043859baebeba5d60d258955750fc1a33c707acc
Guilherme Rodrigues Correa <rodsguilherme@gmail.cm>
2019-05-30 13:56:04 -0300

    Rever cadastro de alimento

--- Api/Controllers/alimento.js
+++ Api/Controllers/alimento.js
@@ -3,42 +3,26 @@ const router = express.Router();
 
 const bancoDeDados = require('./bancoDeDados');
 
-router.get('/', (request, response) => {
-    if (bancoDeDados.conexao.state != 'authenticated')
-        bancoDeDados.conexao.connect();
 
-    bancoDeDados.conexao.query('select (texto) from MensagensDeTeste where id = 4', (erro, resultado) => {
-        if (!erro) {
-            response.json({ Mensagens: resultado });
-        }
-        else {
-            response.json({ Erros: erro });
-        }
-    });
-});
 
 router.post('/', (request, response) => {
-    if (request.body) {
-        if (bancoDeDados.conexao.state != 'authenticated')
-            bancoDeDados.conexao.connect();
-
-        bancoDeDados.conexao.query(`insert into Alimento (Nome, Descrição, Categoria) values('${request.body.mensagem}')`,
-            (erro, sucesso) => {
-                if (!erro) {
+    // Se o user preencher os campos vai entrar no IF
+    if(request.body.nome && request.body.descricao && request.body.categoria) {
+        bancoDeDados.conexao.query(`insert into Alimento (Nome, Descricao, Categoria) values('${response.body.nome}', '${response.body.descricao}', '${response.body.categoria}')`,
+            (erro, resultado) => {
+                if(!erro) // Se não tiver erro
                     response.status(200);
-                    response.send();
-                }
-                else {
-                    response.status(400);
-                    response.send(`Erro ao cadastrar no banco: ${erro}`);
-                }
-            })
-    }
-    else {
-        response.status(400);
-        response.send('Insira uma mensagem!');
-    }
+                else
+                    response.status(400).json({Erro: erro});
+            }
 
+        )}
+          // Se o user não mandar os campos retorna 400
+    else {
+        response.status(400).json({Erro: 'Preencha os campos de Nome, Descrição e Categoria!'});
+        // Manda a request p/ o user
+        response.send();
+    }
 });
 
 module.exports = (api) => api.use('/api/alimento', router);
