ded10ff69eb3d515ae87057292f696a51171a7c8
Cristiano Cunha <contato@cristianoprogramador.com>
2019-05-30 14:04:23 -0300

    Corrige o cadastro de alimento

--- Api/Controllers/alimento.js
+++ Api/Controllers/alimento.js
@@ -7,22 +7,23 @@ const bancoDeDados = require('./bancoDeDados');
 
 router.post('/', (request, response) => {
     // Se o user preencher os campos vai entrar no IF
-    if(request.body.nome && request.body.descricao && request.body.categoria) {
-        bancoDeDados.conexao.query(`insert into Alimento (Nome, Descricao, Categoria) values('${response.body.nome}', '${response.body.descricao}', '${response.body.categoria}')`,
+    if (request.body.nome && request.body.descricao && request.body.categoria >= 0) {
+        bancoDeDados.conexao.query(`insert into Alimento (Nome, Descricao, Categoria) values('${request.body.nome}', '${request.body.descricao}', '${request.body.categoria}')`,
             (erro, resultado) => {
-                if(!erro) // Se não tiver erro
+                if (!erro) // Se não tiver erro
                     response.status(200);
                 else
-                    response.status(400).json({Erro: erro});
+                    response.status(400).json({ Erro: erro });
             }
 
-        )}
-          // Se o user não mandar os campos retorna 400
-    else {
-        response.status(400).json({Erro: 'Preencha os campos de Nome, Descrição e Categoria!'});
-        // Manda a request p/ o user
-        response.send();
+        )
     }
+    // Se o user não mandar os campos retorna 400
+    else
+        response.status(400).json({ Erro: 'Preencha os campos de Nome, Descrição e Categoria!' });
+    // Manda a request p/ o user
+    response.send();
+
 });
 
 module.exports = (api) => api.use('/api/alimento', router);
