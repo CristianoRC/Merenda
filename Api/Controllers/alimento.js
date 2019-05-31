6bdcd25bf7e09bfdb1d5f40eaeaa8b5f6d501c17
Guilherme Rodrigues Correa <rodsguilherme@gmail.cm>
2019-05-31 09:14:24 -0300

    Exclui algumas linhas de código

--- Api/Controllers/alimento.js
+++ Api/Controllers/alimento.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const bancoDeDados = require('./bancoDeDados');
 
+// RETORNA OS DADOS DO ALIMENTO
 router.get('/:Id', (request, response) => {
     const id = request.params.Id;
     bancoDeDados.conexao.query(`select * from Alimento WHERE Id=${id}`, (erro, resultado) => {
@@ -15,6 +16,7 @@ router.get('/:Id', (request, response) => {
     });
 });
 
+// DELETA UM ALIMENTO
 router.delete('/:Id', (request, response) => {
     const id = request.params.Id;
     bancoDeDados.conexao.query(`delete from Alimento WHERE Id=${id}`, (erro, resultado) => {
@@ -26,29 +28,7 @@ router.delete('/:Id', (request, response) => {
             response.status(400).json({ erro: erro });
     });
 });
-
-router.post('/', (request, response) => {
-    // Se o user preencher os campos vai entrar no IF
-    if (request.body.nome && request.body.descricao && request.body.categoria >= 0) {
-        bancoDeDados.conexao.query(`insert into Alimento (Nome, Descricao, Categoria) values('${request.body.nome}', '${request.body.descricao}', '${request.body.categoria}')`,
-            (erro, resultado) => {
-                if (!erro) // Se não tiver erro
-                    response.status(200);
-                else
-                    response.status(400).json({ Erro: erro });
-            }
-
-        )
-    }
-    // Se o user não mandar os campos retorna 400
-    else
-        response.status(400).json({ Erro: 'Preencha os campos de Nome, Descrição e Categoria!' });
-    // Manda a request p/ o user
-    response.send();
-
-});
-
-// Cadastra um novo Alimento 
+// CADASTRA UM NOVO ALIMENTO
 router.post('/', (request, response) => {
     // Se o user preencher os campos vai entrar no IF
     if (request.body.nome && request.body.descricao && request.body.categoria >= 0) {
