445aeffc20fdca49613688df4f1a72a3161315f6
Guilherme Rodrigues Correa <rodsguilherme@gmail.cm>
2019-05-31 20:06:14 -0300

    Deleta Categoria :balloon:

--- Api/Controllers/categoria.js
+++ Api/Controllers/categoria.js
@@ -25,13 +25,13 @@ router.delete('/:id', (request, response) => {
     // Se Deletado estiver em FALSE, a categoria não foi excluida, se estiver em TRUE, a categoria foi 'excluida' 
     bancoDeDados.conexao.query(`select * from Categoria where Deletado = false and Id = ${request.params.id}`, (erro, resultado) => {
         if (resultado.length > 0)
-            response.status(200).send('O id foi incontrado');
+            response.status(200).send('O id foi encontrado e Deletado com sucesso!');
         else
-            response.status(400).send('O id já foi excluido');
+            response.status(404).json({ resultado : 'O id não foi encontrado!' });
         if (!erro)
             bancoDeDados.conexao.query(`update Categoria set Deletado = true where Id = ${request.params.id}`, (deletado, result) => {
                 if (!deletado)
-                    response.status(200).send('Deletado com sucesso');
+                    response.status(200).send();
 
                 else
                     response.status(400).json({ deletado: deletado });
