fce6f5a30473313af24c9af6bdb5e4d6c5a791d3
dionevoss <dionevoss@gmail.com>
2019-06-11 20:36:18 -0300

    Corrige insere merenda

--- Api/Controllers/merenda.js
+++ Api/Controllers/merenda.js
@@ -4,9 +4,15 @@ const router = express.Router();
 const bancoDeDados = require('./bancoDeDados');
 
 router.post('/', (request, response) => {
-    if (request.body.data && request.body.observacoes && request.body.idalimento) {
-        bancoDeDados.conexao.query(`select * from Alimento where Id=${request.body.idalimento}`, (erro, resultado) => {
-            if (resultado.length > 0) {
+    if (request.body.data && request.body.observacoes && request.body.alimento) {
+        let sql = "SELECT nome FROM Alimento WHERE id in(";
+        for (let alimentos of request.body.alimento) {
+            sql = sql + `${alimentos},`
+        }
+        sql = sql.substr(0, (sql.length - 1));
+        sql = sql + ')';
+        bancoDeDados.conexao.query(sql, (erro, resultado) => {
+            if (resultado.length > 2) {
                 bancoDeDados.conexao.query(`insert into Cardapio (Data, Observacoes)
                     values('${request.body.data}', '${request.body.observacoes}')`, (erro, resultado) => {
                         if (!erro)
@@ -14,17 +20,20 @@ router.post('/', (request, response) => {
                         else
                             response.status(400).json({ erro: erro });
                     });
-                bancoDeDados.conexao.query(`insert into Cardapio_Alimento (Data, IdAlimento)
-                    values('${request.body.data}', '${request.body.idalimento}')`, (erro, resultado) => {
-                        if (!erro)
-                            response.status(200).json({ resultado: 'Dados inseridos com sucesso!' });
-                        else
-                            response.status(400).json({ erro: erro });
-                    });
-
+                let sql = "insert into Cardapio_Alimento (Data, IdAlimento) values";
+                for (let alimentos of request.body.alimento) {
+                    sql = sql + `('${request.body.data}', ${alimentos}),`
+                }
+                sql = sql.substr(0, (sql.length - 1));
+                bancoDeDados.conexao.query(sql, (erro, resultado) => {
+                    if (!erro)
+                        response.status(200).json({ resultado: 'Dados inseridos com sucesso!' });
+                    else
+                        response.status(400).json({ erro: erro });
+                })
             }
             else
-                response.status(400).json({ erro: 'O id do alimendo não existe na tabela de alimentos!' });
+                response.status(400).json({ erro: 'Um ou mais ID(s) inseridos não existem na tabela Alimento!' });
         });
     }
     else
